5676360fd7a253ce9fc934cf9777ab17e3feccc2
Adams009 <olaadam003@gmail.com>
2024-05-01 03:34:52 -0700

    good

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,14 @@ index
 from flask import jsonify
 from api.v1.views import app_views
 
+m models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
@@ -21,3 +29,20 @@ def status():
     resp.status_code = 200
 
     return resp
+
+
+@app_views.route('/stats')
+def get_stats():
+    '''Gets the number of objects for each type.
+    '''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
